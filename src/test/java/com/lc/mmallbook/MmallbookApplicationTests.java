1a411174e5867b55799624d4fcf0e6af62f30562
741263802 <741263802@qq.com>
2020-11-19 13:15:03 +0800

    Merge branch 'master' of https://github.com/qq741263802/mmallbook
    # Conflicts:
    #	src/test/java/com/lc/mmallbook/MmallbookApplicationTests.java

--- src/test/java/com/lc/mmallbook/MmallbookApplicationTests.java
+++ src/test/java/com/lc/mmallbook/MmallbookApplicationTests.java
@@ -1,25 +1,23 @@
 package com.lc.mmallbook;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.lc.mmallbook.common.ServerResponse;
 import com.lc.mmallbook.pojo.Product;
 import com.lc.mmallbook.service.IProductService;
-import com.lc.mmallbook.util.LbReptileUtil;
 import com.lc.mmallbook.util.excel.ExcelUtil;
 import com.lc.mmallbook.util.excel.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.math.BigDecimal;
-import java.util.Random;
 import java.util.UUID;
+import java.sql.*;
+import java.util.*;
+import java.lang.*;
+//import org.apache.regexp.*;
+import org.json.*;
+
 
 @SpringBootTest
  class MmallbookApplicationTests {
@@ -60,7 +58,7 @@ import java.util.UUID;
 
 
 	@Test
-	void setExcel() throws IOException {
+	void setExcel() throws IOException, SQLException {
 
       User user=new User();
       user.setAge(20);
@@ -68,7 +66,39 @@ import java.util.UUID;
       String filename= "D:/user1.xls";
       ExcelUtil.writeExcel(filename,user);
 
+        String drive = "org.gjt.mm.mysql.Driver";
+        String url = "jdbc:mysql://81.71.4.237:3306/";
+        String dbName = "mmall";
+        String user1 = "root";
+        String pass = "qq18924260161";
+        String history = "";
+        String response = "";
+        String failuer = "";
+        String query ="select * from mmall_user";
+        Connection Mycon = null;
+        Statement Mystmt = null;
+        ResultSet Myrset = null;
+        try{
+            Mycon = DriverManager.getConnection(url+dbName, user1, pass);
 
+        }	catch(SQLException e){
+
+        }
+        Mystmt = Mycon.createStatement();
+        Myrset = Mystmt.executeQuery(query);
+        while (Myrset.next()){
+            history = Myrset.getString(2);
+           // String name = Myrset.getString(2);
+            System.out.println(history);
+        }
+        Myrset.close();
+        Mystmt.close();
+
+//        if(history == "")
+//        {
+////            Failure = true;
+////            FailureMessage = "连接数据库失败";
+//        }
 
 
 	}
